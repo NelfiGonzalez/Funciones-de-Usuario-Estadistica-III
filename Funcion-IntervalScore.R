1038025567704e5afc56c61d943bb6873b5b2216
NelfiGonzalez <100295079+NelfiGonzalez@users.noreply.github.com>
2024-10-22 13:42:12 -0500

    Update Funcion-IntervalScore.R

--- Funcion-IntervalScore.R
+++ Funcion-IntervalScore.R
@@ -7,6 +7,14 @@
 #However, if the observation falls outside the interval, the penalty applies, 
 #with the penalty proportional to how far the observation is outside the interval.
 #-------------------------------------------------------------------------------------------------------------------------------------
+##Argumentos:
+# real: un valor o vector de valores observados de la serie en los periodos de pronostico
+# LimInf,LimSup: valores o vectores de valores de los limites inferior y superior de los intervalos de prediccion correspondientes
+# alpha: valor correspondiente al I.P (1-alpha)100%
+
+## Valor arrojado: La funcion produce un numero real positivo correspondiente al Score del intervalo (en el caso de un solo IP) 
+#  o al promedio de los Scores de los intervalos de prediccion (en el caso de m IPs)
+#-------------------------------------------------------------------------------------------------------------------------------------
 
 IntervalScore=function(real,LimInf,LimSup,alpha){
 S.alpha=(LimSup-LimInf)+(2/alpha)*(LimInf-real)*ifelse(real<LimInf,1,0)+(2/alpha)*(real-LimSup)*ifelse(real>LimSup,1,0)
